c9b3f27a0e595c7419a455c0e58aafb02e0b5ba9
alfonsoperezs <fonpesan@gmail.com>
2024-03-25 16:13:24 +0100

    Class Crypto. Implemented main.py

--- crypto.py
+++ crypto.py
@@ -1,10 +1,16 @@
 from bs4 import BeautifulSoup 
 import requests
 
-def crypto_obtainValue():
-    url = requests.get(f'https://awebanalysis.com/es/coin-details/bitcoin/')
-    soup = BeautifulSoup(url.content, 'html.parser') # Crea un objeto BeautifulSoup para analizar el contenido HTML de la página web.
-    result = soup.find('td', {'class': 'text-larger text-price'})
-    format_result = result.text
+class Crypto :
+    def __init__(self, cryptoName) :
+        self.cryptoName = cryptoName
     
-    return format_result
+    # Obtain de value of the crypto    
+    def crypto_obtainValue(self):
+        url = requests.get(f'https://awebanalysis.com/es/coin-details/{self.cryptoName}/')
+        soup = BeautifulSoup(url.content, 'html.parser') # analize HTML content
+        result = soup.find('td', {'class': 'text-larger text-price'})
+        format_result = result.text
+        
+        return format_result
+
--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 from config import *
 from tw import *
+from crypto import *
+import datetime
+import time
 
+tw = Tw(BAERER_TOKEN,API_KEY,API_SECRET,ACCESS_TOKEN,ACCESS_SECRET)
+btc = Crypto("bitcoin")
+
+
+while True :
+    currentTime = datetime.datetime.now()
+    if currentTime.minute == 0:
+        btcVal = btc.crypto_obtainValue()
+        tw.postTweet("The #Bitcoin is at " + btcVal + " now")
+        time.sleep(60)
+
+
+    
 
 
 
